1453f31097804ce506f6665e5589b2a4a8015169
sdana <seth.dana@gmail.com>
2018-07-10 14:35:04 -0500

    I think MVP is met, in testing phase now

--- src/scripts/buildMovieWall.js
+++ src/scripts/buildMovieWall.js
@@ -17,13 +17,13 @@ const movieWall = Object.create({},{
                         let movieTitle = $(`<h3 id='title${movie.id}'>${movie.title}</h3>`)
                         let movieDesc = $(`<p id='desc${movie.id}'>${movie.desc}</p>`)
                         let movieDur = $(`<p id='dur${movie.id}'>${movie.duration}</p>`)
-                        const deleteBtn = $("<button id='delete-btn'>Delete Movie</button>")
-                        const editBtn = $("<button id='edit-btn'>Edit Details</button>")
+                        const deleteBtn = $(`<button id='delete-btn${movie.id}'>Delete Movie</button>`)
+                        const editBtn = $(`<button id='edit-btn${movie.id}'>Edit Details</button>`)
                         const watchedCheck = $("<input type='checkbox' value='watched'>")
                         watchedCheck.click(e => {
                             console.log(e.target.nextSibling)
                             const targetId = e.target.parentNode.id
-                            markWatched.markAsWatched(targetId)
+                            this.markAsWatched(targetId)
                         })
                         editBtn.click(e =>{
                             const targetId = e.target.parentNode.id
@@ -31,6 +31,9 @@ const movieWall = Object.create({},{
                             const movieDesc = $(`#desc${targetId}`).text()
                             const movieDur = $(`#dur${targetId}`).text()
                             this.editMovieInfo(targetId, movieTitle, movieDesc, movieDur)
+                        })
+                        deleteBtn.click(e => {
+                            this.deleteMovie(e.target.parentNode.id)
                         })
                         const movieContainer = $(`<div class='movie-container' id='${movieID}'>`)
                         movieContainer.append(watchedCheck).append(movieTitle).append(movieDesc).append(movieDur).append(editBtn).append(deleteBtn)
@@ -43,9 +46,6 @@ const movieWall = Object.create({},{
     },
     markAsWatched: {
         value: function(movieId){
-            console.log(movieTitle)
-            console.log(movieDesc)
-            console.log(movieDur)
             api.markAsWatched(movieId).then(response =>{
             this.buildMovieWall()
             })
@@ -53,8 +53,8 @@ const movieWall = Object.create({},{
     },
     editMovieInfo: {
         value: function (targetId, movieTitle, movieDesc, movieDur){
-            $("#edit-btn").remove()
-            $("#delete-btn").remove()
+            $(`#edit-btn${targetId}`).remove()
+            $(`#delete-btn${targetId}`).remove()
             const submitBtn = $("<button>Submit</button>")
             const titleEdit = $(`<input type='text' value='${movieTitle}'>`)
             const descEdit = $(`<input type='text' value='${movieDesc}'>`)
@@ -68,6 +68,13 @@ const movieWall = Object.create({},{
                     this.buildMovieWall()
                 })
 
+            })
+        }
+    },
+    deleteMovie: {
+        value: function(movieId){
+            api.deleteMovie(movieId).then(response =>{
+                this.buildMovieWall()
             })
         }
     }
